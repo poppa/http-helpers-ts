e2748ed1255af54b726dd19dbf0a7006a9a9c1be
Pontus Östlund <ponost@roxen.com>
2022-03-19 09:20:58 +0100

    HttpError additions

---
+++ http-error/src/http-error.spec.ts
@@ -0,0 +1,78 @@
+import 'jest'
+import { HttpError } from './http-error'
+
+test('No second constructor argument should give default status text', () => {
+  expect(new HttpError(201).statusText).toEqual('Created')
+})
+
+test('Second constructor arg should treat a string as status text', () => {
+  const err = new HttpError(201, 'Made it')
+  expect(err.statusCode).toEqual(201)
+  expect(err.statusText).toEqual('Made it')
+})
+
+test('Second constructor arg with object with status text should use that', () => {
+  const err = new HttpError(201, { statusText: 'Made it' })
+  expect(err.statusCode).toEqual(201)
+  expect(err.statusText).toEqual('Made it')
+})
+
+test('Second constructor arg with object with message should use that', () => {
+  const err = new HttpError(201, { message: 'Error message' })
+  expect(err.statusCode).toEqual(201)
+  expect(err.message).toEqual('Error message')
+  expect(err.statusText).toEqual('Created')
+})
+
+test('Second constructor arg with object with both message and status text should use those', () => {
+  const err = new HttpError(201, {
+    statusText: 'Made it',
+    message: 'Error message',
+  })
+  expect(err.statusCode).toEqual(201)
+  expect(err.message).toEqual('Error message')
+  expect(err.statusText).toEqual('Made it')
+})
+
+test('statusCode should return what was given to constructor', () => {
+  const e = new HttpError(200)
+  expect(e.statusCode).toEqual(200)
+})
+
+test('statusCode should be read-only', () => {
+  const e = new HttpError(200)
+  expect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    e.statusCode = 201
+  }).toThrow()
+})
+
+test('statusText should be read-only', () => {
+  const e = new HttpError(200)
+  expect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    e.statusText = 'Nono'
+  }).toThrow()
+})
+
+test('fullStatusMessage should always return "XXX Text"', () => {
+  const err1 = new HttpError(201)
+  expect(err1.fullStatusMessage).toEqual('201 Created')
+
+  const err2 = new HttpError(201, 'Made it')
+  expect(err2.fullStatusMessage).toEqual('201 Made it')
+
+  const err3 = new HttpError(201, { statusText: 'Made it' })
+  expect(err3.fullStatusMessage).toEqual('201 Made it')
+
+  const err4 = new HttpError(201, { message: 'Error message' })
+  expect(err4.fullStatusMessage).toEqual('201 Created')
+
+  const err5 = new HttpError(201, {
+    message: 'Error message',
+    statusText: 'Made it',
+  })
+  expect(err5.fullStatusMessage).toEqual('201 Made it')
+})
--- http-error/src/http-error.ts
+++ http-error/src/http-error.ts
@@ -1,21 +1,78 @@
 import { getStatusText } from '@poppanator/http-constants'
+import type { Maybe } from './_type-types'
 
+export type HttpErrorConstuctorArgs = {
+  /** Status text */
+  statusText?: string
+  /** Error message */
+  message?: string
+}
+
+/**
+ * A class representing a HTTP error with status code and status text
+ */
 export class HttpError extends Error {
   private readonly _code: number
-  private readonly _text: string
+  private readonly _text: Maybe<string>
 
-  constructor(statusCode: number, statusText?: string, message?: string) {
-    super(message ?? statusText ?? getStatusText(statusCode))
+  /**
+   *
+   * @param statusCode
+   *  The HTTP status code, e.g. `301`, `400`, `404`, `500` etc
+   * @param statusTextOrArgs -
+   *   - If `undefined` the status text will be automatically resolved to the
+   *     `statusCodes`'s corresponding status text, e.g. `OK` for `200`,
+   *     `Not Found` for `404`, `Internal Server Error` for `500` etc.
+   *
+   *   - If a `string` the value will be used as status text
+   *   - If a {@link HttpErrorConstuctorArgs} object the status text and message
+   *     will be potentially revolved from there.
+   */
+  constructor(
+    statusCode: number,
+    statusTextOrArgs?: string | HttpErrorConstuctorArgs
+  ) {
+    super()
+
+    let text: Maybe<string>
+    let message: Maybe<string>
+
+    if (typeof statusTextOrArgs === 'undefined') {
+      text = getStatusText(statusCode)
+    } else if (typeof statusTextOrArgs === 'string') {
+      text = statusTextOrArgs
+    } else {
+      message = statusTextOrArgs.message
+      text = statusTextOrArgs.statusText ?? getStatusText(statusCode)
+    }
 
     this._code = statusCode
-    this._text = this.message
+    this._text = text
+
+    if (message) {
+      this.message = message
+    } else {
+      this.message = `${statusCode} ${this._text ?? ''}`
+    }
   }
 
+  /** Returns the HTTP status code */
   public get statusCode(): number {
     return this._code
   }
 
+  /**
+   * Returs the HTTP status text, or an empty string if non was set or resolved
+   */
   public get statusText(): string {
-    return this._text
+    return this._text ?? ''
+  }
+
+  /**
+   * Returns the complete HTTP status string, like `200 OK`, `404 Not Found`
+   * e t c.
+   */
+  public get fullStatusMessage(): string {
+    return `${this._code} ${this.statusText}`
   }
 }
