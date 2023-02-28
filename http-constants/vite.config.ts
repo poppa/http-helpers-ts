967a15b4ec4e10eeba39a88e3ec6e84f447966c5
Pontus Östlund <ponost@roxen.com>
2023-02-28 15:36:13 +0100

    chore: Use Vite to build the http-constants package

---
+++ http-constants/vite.config.ts
@@ -0,0 +1,30 @@
+import { join, resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+const base = join(__dirname, 'src')
+
+export default defineConfig({
+  plugins: [
+    dts({ entryRoot: './src/', tsConfigFilePath: './tsconfig.build.json' }),
+  ],
+  build: {
+    sourcemap: true,
+    minify: 'terser',
+    lib: {
+      formats: ['es', 'cjs'],
+      entry: {
+        index: resolve(base, 'index.ts'),
+        codes: resolve(base, 'codes.ts'),
+        texts: resolve(base, 'texts.ts'),
+        tules: resolve(base, 'tuples.ts'),
+        methods: resolve(base, 'methods.ts'),
+        headers: resolve(base, 'headers', 'index.ts'),
+      },
+      name: '@poppanator/http-constants',
+      fileName: (format, entryName) => {
+        return `${entryName}.${format === 'cjs' ? 'cjs' : 'js'}`
+      },
+    },
+  },
+})
