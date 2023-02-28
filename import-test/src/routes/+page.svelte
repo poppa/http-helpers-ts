c391ad92a2fa38d1d5ceb1ac8770591cd9f9a563
Pontus Östlund <ponost@roxen.com>
2023-02-28 15:35:41 +0100

    chore: Added test app

---
+++ import-test/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { MultiStatus } from '@poppanator/http-constants/codes';
+	import { Get, Head } from '@poppanator/http-constants/methods';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Multistatus: {MultiStatus}</p>
+<p>Get: {Get}</p>
+<p>Head: {Head}</p>
